6e6c49198952cfc8381963379997336341baac8f
dallasclarke <dallas.clarke622@gmail.com>
2021-01-23 12:43:14 -0800

    Setup

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,27 @@
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searched: [],
+};
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state, popular: action.payload.popular };
+    default:
+      return { ...state };
+  }
+};
+
+// Action Creator
+const fetchGames = (userData) => {
+  return {
+    type: "FETCH_GAMES",
+    payload: userData,
+  };
+};
+
+fetchGames({ user: "name" });
+
+export default gamesReducer;
