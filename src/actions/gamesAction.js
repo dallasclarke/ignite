5c525eb26053c3ef59c981ac977d8b04931558db
dallasclarke <dallas.clarke622@gmail.com>
2021-01-24 12:04:13 -0800

    Fetching newGames, upcomingGames & popular

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 // Action Creator
 export const loadGames = () => async (dispatch) => {
   // Fetch AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(upcomingGamesURL());
+  const upcomingData = await axios.get(newGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
